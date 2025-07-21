e8dc7d0079e4074b596ef74aa49b2192406b1408
Leffin <levfag@gmail.com>
2025-07-21 08:39:05 +0700

    Создан проект

---
+++ Millionaire/MillionaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillionaireApp.swift
+//  Millionaire
+//
+//  Created by Effin Leffin on 21.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MillionaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
