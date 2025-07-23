4eb62895f680e3631edc9f4a874a7629b9c9e99b
Leffin <levfag@gmail.com>
2025-07-23 14:25:41 +0700

    #9 Создана модель подсказок

---
+++ Millionaire/LifelineModel.swift
@@ -0,0 +1,14 @@
+//
+//  LifelineModel.swift
+//  Millionaire
+//
+//  Created by Effin Leffin on 23.07.2025.
+//
+
+import Foundation
+
+enum Lifeline: Hashable {
+    case fiftyFifty
+    case audience
+    case callAFriend
+}
