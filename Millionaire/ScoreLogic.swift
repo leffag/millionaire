1f84a74cbd3e259235335c67dddef1b7135cd2a1
Leffin <levfag@gmail.com>
2025-07-23 11:45:43 +0700

    #11 Добавлена логика подсчета игры

---
+++ Millionaire/ScoreLogic.swift
@@ -0,0 +1,34 @@
+//
+//  ScoreLogic.swift
+//  Millionaire
+//
+//  Created by Effin Leffin on 23.07.2025.
+//
+
+import Foundation
+
+enum ScoreLogic {
+    static let questionValues: [Int] = [
+        100,
+        100,
+        100,
+        200,
+        500,
+        1000,
+        2000,
+        4000,
+        8000,
+        16000,
+        32000,
+        61000,
+        125000,
+        250000,
+        500000
+    ]
+    
+    static let checkpointIndices: [Int] = [4, 9, 14]
+    
+    static func findClosestCheckpointScore(questionIndex: Int) -> Int {
+        return checkpointIndices.last { $0 < questionIndex } ?? 0
+    }
+}
