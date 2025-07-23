f68ee6728af329e00f1a464ed4d084000b2e1b0b
Leffin <levfag@gmail.com>
2025-07-23 17:17:01 +0700

    Update GameSession.swift
    Добавлены подсказки для текущей сессии игры

--- Millionaire/GameSession.swift
+++ Millionaire/GameSession.swift
@@ -13,9 +13,31 @@ struct GameSession {
     var isFinished: Bool
     var currentQuestionIndex: Int
     var score: Int
+    var lifelines: Set<Lifeline>
     
     var currentQuestion: Question {
         questions[currentQuestionIndex]
+    }
+    
+    init?(
+        questions: [Question],
+        isFinished: Bool = false,
+        currentQuestionIndex: Int = 0,
+        score: Int = 0,
+        lifelines: Set<Lifeline> = [.fiftyFifty, .callAFriend, .audience]
+    ) {
+        guard
+            questions.count == 15,
+            0..<15 ~= currentQuestionIndex
+        else {
+            return nil
+        }
+        
+        self.questions = questions
+        self.isFinished = isFinished
+        self.currentQuestionIndex = currentQuestionIndex
+        self.score = score
+        self.lifelines = lifelines
     }
     
     mutating func answer(answer: String) {
@@ -24,7 +46,7 @@ struct GameSession {
         }
         
         if answer == currentQuestion.correctAnswer {
-            score = ScoreLogic.questionValues[currentQuestionIndex]
+            score += ScoreLogic.questionValues[currentQuestionIndex]
             currentQuestionIndex += 1
             
             let hasNextQuestion = currentQuestionIndex + 1 < questions.count
@@ -38,6 +60,18 @@ struct GameSession {
         } else {
             score = ScoreLogic.findClosestCheckpointScore(questionIndex: currentQuestionIndex)
             isFinished = true
+        }
+    }
+    
+    mutating func useLifeline(lifeline: Lifeline) {
+        guard !isFinished else {
+            return
+        }
+        
+        if lifelines.contains(lifeline) {
+            lifelines.remove(lifeline)
+        } else {
+            return
         }
     }
 }
